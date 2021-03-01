807da9a8becc98c708dc20647091fedb47281b81
Ziqian Qiu <ziqian.qiu@mail.utoronto.ca>
2021-02-28 23:36:34 -0500

    figure.py added

--- figure.py
+++ figure.py
@@ -1,9 +1,34 @@
 import turtle
 g = turtle.Turtle()
 g.shape("turtle")
-g.speed(0)
+g.speed(1)
 counter = 0
 
+g.forward(80)
+g.left(72)
+g.forward(80)
+g.left(72)
+g.forward(80)
+g.left(72)
+g.forward(80)
+g.left(72)
+g.forward(80)
+g.left(105)
+g.forward(80)
+g.left(72)
+g.forward(80)
+g.left(72)
+g.forward(80)
+g.left(72)
+g.forward(80)
+g.left(72)
+g.forward(80)
+g.left(0)
+g.right(0)
+g.left(0)
+g.right(0)
+g.left(0)
+g.right(0)
 
 
 def draw_pentagon(g):
@@ -11,17 +36,18 @@ def draw_pentagon(g):
     g.left(72)
 
 
-while counter <= 120:
-    g.forward(80)
-    g.left(72)
-    g.forward(80)
-    g.left(72)
-    g.forward(80)
-    g.left(72)
-    g.forward(80)
-    g.left(72)
-    g.forward(80)
-    g.left(105)
-    counter += 1
+
+# while counter <= 120:
+#     g.forward(80)
+#     g.left(72)
+#     g.forward(80)
+#     g.left(72)
+#     g.forward(80)
+#     g.left(72)
+#     g.forward(80)
+#     g.left(72)
+#     g.forward(80)
+#     g.left(105)
+#     counter += 1
 
 
